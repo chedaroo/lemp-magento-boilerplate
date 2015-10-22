c15bd3f768ed2cb8f64c5fcadac5c37cafcf27ad
Rich <rich@me.com>
2015-10-22 16:56:40 +0100

    Fixed php my admin

--- bin/inc/common-install-packages.sh
+++ bin/inc/common-install-packages.sh
@@ -11,15 +11,16 @@ ln -fs $PROJECT_ROOT/conf/nginx.conf /etc/nginx/nginx.conf
 
 # Install MySQL
 echo -e "\x1b[92m\x1b[1mInstalling MySQL...\x1b[0m\x1b[21m"
-apt-get -q -y install mysql-server mysql-client  #> /dev/null
+apt-get -q -y install mysql-server mysql-client  > /dev/null
 
 # Install additional required packages
-apt-get install -y vim git curl #> /dev/null
-apt-get install -y redis-server php5-dev php5-cli php5-mysql php5-mcrypt php5-gd php5-curl php5-tidy php-pear php-apc  #> /dev/nul
+echo -e "\x1b[92m\x1b[1mInstalling PHP and additional packages...\x1b[0m\x1b[21m"
+apt-get install -y vim git curl > /dev/null
+apt-get install -y redis-server php5-dev php5-cli php5-mysql php5-mcrypt php5-gd php5-curl php5-tidy php-pear php-apc  > /dev/nul
 
 # Install Redis server for Backend and Session caching
 echo -e "\x1b[92m\x1b[1mInstalling Redis...\x1b[0m\x1b[21m"
-apt-get install -y redis-server  #> /dev/null
+apt-get install -y redis-server  > /dev/null
 pecl install redis
 echo "extension=redis.so" > /etc/php5/mods-available/redis.ini
 php5enmod redis
@@ -30,4 +31,10 @@ apt-get install -y phpmyadmin
 if [ ! -e "$RSYNC_TARGET/www/phpmyadmin" ]; then
   # Create symbolic link from fileshare, composer will need this to validate magento installation
   ln -s /usr/share/phpmyadmin/ $RSYNC_TARGET/www
+  # Create config backup
+  cp /etc/phpmyadmin/config.inc.php /etc/phpmyadmin/config.old.inc.php
+  # Allow no password for root
+  PMA_AllowNoPassword_Find="AllowNoPassword"
+  PMA_AllowNoPassword_Replace="\$cfg['Servers'][\$i]['AllowNoPassword'] = true;"
+  sed -i "s/.*$PMA_AllowNoPassword_Find.*/$PMA_AllowNoPassword_Replace/" /etc/phpmyadmin/config.inc.php # Find in line and replace whole line
 fi
